1e0f48f5e84c5941b06d98bf889cc7be68b537e0
yana.loginova <yana.loginova@cix.csi.cuny.edu>
2019-05-09 05:47:56 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -39,7 +39,11 @@ end
     def using_delete(array,string)
       array.delete(string)
     end       
-    def 
+    def using_delete_at(array, int)
+      array.delete(int)
+    end
+      
+      
     
     
     
