71d60c6d6cf9b827b126656e0df876ee28a39aa6
yana.loginova <yana.loginova@cix.csi.cuny.edu>
2019-05-09 05:25:35 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,28 @@
+def using_push(array, string)
+  array.push(string)
+end
+
+def using_unshift (array, string)
+  array.unshift(string) 
+end
+
+def using_pop (array)
+  array.pop()
+end
+
+def pop_with_args(array)
+  return array.pop(2)
+end
+def using_shift(array)
+  array.shift(0)
+end
+def using_shift(array)
+  return array.shift(3)
+end
+def using_concat(arg1,arg2)
+  arg1.concat(arg2)
+end
+  
+  
+  
+  
